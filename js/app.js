e26e20241577c728dc97a726a68e18386e3997c2
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-11-10 13:48:32 +0100

    Submenus del nav Diseñado, falta el js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,33 @@
 //variables
 const header__burguer = document.querySelector(".header__burguer");
 const nav = document.querySelector(".nav");
-
 const nav__close = document.querySelector("#nav__close");
+
+// VARIABLES submenu
+
+
 //Eventos
+
+
 header__burguer.addEventListener("click", showMenu);
 nav__close.addEventListener("click", hiddenMenu);
+
+
+// SUBMENUS EVENTOS
+
+
+
 //funciones
+
+
 function showMenu() {
   nav.classList.toggle("activo");
 }
+
 function hiddenMenu() {
   nav.classList.toggle("activo");
 }
+
+
+// SUBMENUS FUNCIONES
+
