e2d3a305e2dd37733363fb90cbb9c89a11deb973
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-11-10 16:34:41 +0100

    Submenub del nav realizado con js, falta por completar algunos submenus

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ const nav__close = document.querySelector("#nav__close");
 
 // VARIABLES submenu
 
-
+const submenuBtn = document.querySelectorAll('.submenu__btn')
+const submenu__arrowleft = document.querySelectorAll('.submenu__title')
 //Eventos
 
 
@@ -16,8 +17,25 @@ nav__close.addEventListener("click", hiddenMenu);
 // SUBMENUS EVENTOS
 
 
+  submenuBtn.forEach((e) => {
+    console.log(e);
+    e.addEventListener('click', ()=>{
+      let menuHermano = e.nextElementSibling
+      menuHermano.classList.add('activo2')
+    })
+  });
 
+  submenu__arrowleft.forEach((ele) =>{
+    console.log(ele);
+    ele.addEventListener('click', ()=>{
+      let subMenuCerrar = ele.parentElement.parentElement
+      console.log(subMenuCerrar);
+      subMenuCerrar.classList.remove('activo2')
+    })
+  })
+  
 //funciones
+
 
 
 function showMenu() {
@@ -27,7 +45,3 @@ function showMenu() {
 function hiddenMenu() {
   nav.classList.toggle("activo");
 }
-
-
-// SUBMENUS FUNCIONES
-
