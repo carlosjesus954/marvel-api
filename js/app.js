5fcb26204f0f51d7a73b207d457d824d9efe1b79
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-11-14 19:53:29 +0100

    Carrusel forma manual terminada / falta la automatica

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 const header__burguer = document.querySelector(".header__burguer");
 const nav = document.querySelector(".nav");
 const nav__close = document.querySelector("#nav__close");
+const carrusel__li = document.querySelectorAll('.carrusel__li')
+const carrusel__containerimg = document.querySelector('.carrusel__containerimg')
+const carrusel__globaltext = document.querySelector('.carrusel__globaltext')
 
 // VARIABLES submenu
 
@@ -13,12 +16,31 @@ const submenu__arrowleft = document.querySelectorAll('.submenu__title')
 header__burguer.addEventListener("click", showMenu);
 nav__close.addEventListener("click", hiddenMenu);
 
+// Carrusel events
+
+carrusel__li.forEach((cadaPunto , i) => {
+  carrusel__li[i].addEventListener('click', ()=>{
+    let posicion = i
+    // guardamos la posicion y dependiendo de la posicion que estemos tendra valor en la operacion
+    let operacion = posicion * -50
+    console.log(operacion);
+    //cuando la posicion es 0 el transformX es 0
+    carrusel__containerimg.style.transform = `translateX(${operacion}%)`
+    carrusel__globaltext.style.transform = `translateX(${operacion}%)`
+    //cuando posicion es 1 transformX es -50%
+    
+   carrusel__li.forEach((cadaPunto , i) =>{
+    carrusel__li[i].classList.remove('carrusel__li--activo')
+   })
+   carrusel__li[i].classList.add('carrusel__li--activo')
+    
+  })
+});
 
 // SUBMENUS EVENTOS
 
 
   submenuBtn.forEach((e) => {
-    console.log(e);
     e.addEventListener('click', ()=>{
       let menuHermano = e.nextElementSibling
       menuHermano.classList.add('activo2')
@@ -26,10 +48,8 @@ nav__close.addEventListener("click", hiddenMenu);
   });
 
   submenu__arrowleft.forEach((ele) =>{
-    console.log(ele);
     ele.addEventListener('click', ()=>{
       let subMenuCerrar = ele.parentElement.parentElement
-      console.log(subMenuCerrar);
       subMenuCerrar.classList.remove('activo2')
     })
   })
