a3a06ed5f9bcbb96c863c928a2aa7b0574fc6e4f
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-11-16 13:25:27 +0100

    Haciendo responsive el nav y el carrusel, he desabilitado el carrusel automatico y he implementado los nuevos elementos en el carrusel..

--- js/app.js
+++ js/app.js
@@ -1,77 +1,78 @@
 //variables
-const header__burguer = document.querySelector(".header__burguer");
+const headerBurguer = document.querySelector(".header__burguer");
 const nav = document.querySelector(".nav");
-const nav__close = document.querySelector("#nav__close");
-const carrusel__li = document.querySelectorAll('.carrusel__li')
-const carrusel__containerimg = document.querySelector('.carrusel__containerimg')
-const carrusel__globaltext = document.querySelector('.carrusel__globaltext')
+const navClose = document.querySelector("#nav__close");
+const carruselLi = document.querySelectorAll(".carrusel__li");
+const carruselContainerimg = document.querySelector(".carrusel__containerimg");
+const carruselGlobaltext = document.querySelector(".carrusel__globaltext");
 
 // VARIABLES submenu
 
-const submenuBtn = document.querySelectorAll('.submenu__btn')
-const submenu__arrowleft = document.querySelectorAll('.submenu__title')
+const submenuBtn = document.querySelectorAll(".submenu__btn");
+const submenu__arrowleft = document.querySelectorAll(".submenu__title");
 //Eventos
 
-
-header__burguer.addEventListener("click", showMenu);
-nav__close.addEventListener("click", hiddenMenu);
+headerBurguer.addEventListener("click", showMenu);
+navClose.addEventListener("click", hiddenMenu);
 
 // Carrusel events
-setTimeout(function(){
-  console.log('object');
-  carrusel__li.forEach((cadaPunto, i) =>{
-    // whil
-    let posicion = i
-    let operacion = posicion * -50
-    carrusel__containerimg.style.transform = `translateX(${operacion}%)`
-    carrusel__globaltext.style.transform = `translateX(${operacion}%)`
 
-    carrusel__li.forEach((cadaPunto , i) =>{
-      carrusel__li[i].classList.remove('carrusel__li--activo')
-     })
-     carrusel__li[i].classList.add('carrusel__li--activo')
-  })
-}, 4000)
-
-
-carrusel__li.forEach((cadaPunto , i) => {
-  carrusel__li[i].addEventListener('click', ()=>{
-    let posicion = i
+carruselLi.forEach((cadaPunto, i) => {
+  carruselLi[i].addEventListener("click", () => {
+    let posicion = i;
     // guardamos la posicion y dependiendo de la posicion que estemos tendra valor en la operacion
-    let operacion = posicion * -50
+    let operacion = posicion * -20;
     //cuando la posicion es 0 el transformX es 0
-    carrusel__containerimg.style.transform = `translateX(${operacion}%)`
-    carrusel__globaltext.style.transform = `translateX(${operacion}%)`
+    carruselContainerimg.style.transform = `translateX(${operacion}%)`;
+    carruselGlobaltext.style.transform = `translateX(${operacion}%)`;
     //cuando posicion es 1 transformX es -50%
-    
-   carrusel__li.forEach((cadaPunto , i) =>{
-    carrusel__li[i].classList.remove('carrusel__li--activo')
-   })
-   carrusel__li[i].classList.add('carrusel__li--activo')
-    
-  })
+
+    carruselLi.forEach((cadaPunto, i) => {
+      carruselLi[i].classList.remove("carrusel__li--activo");
+    });
+    carruselLi[i].classList.add("carrusel__li--activo");
+  });
 });
 
 // SUBMENUS EVENTOS
 
-
-  submenuBtn.forEach((e) => {
-    e.addEventListener('click', ()=>{
-      let menuHermano = e.nextElementSibling
-      menuHermano.classList.add('activo2')
-    })
+submenuBtn.forEach((e) => {
+  e.addEventListener("click", () => {
+    let menuHermano = e.nextElementSibling;
+    menuHermano.classList.add("activo2");
   });
+});
 
-  submenu__arrowleft.forEach((ele) =>{
-    ele.addEventListener('click', ()=>{
-      let subMenuCerrar = ele.parentElement.parentElement
-      subMenuCerrar.classList.remove('activo2')
-    })
-  })
-  
+submenu__arrowleft.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    let subMenuCerrar = ele.parentElement.parentElement;
+    subMenuCerrar.classList.remove("activo2");
+  });
+});
+
 //funciones
+// setTimeout(function () {
+//   carruselLi.forEach((cadaPunto, i) => {
+//     let posicion = i;
+//     let operacion = posicion * -20;
+//     carruselContainerimg.style.transform = `translateX(${operacion}%)`;
+//     carruselGlobaltext.style.transform = `translateX(${operacion}%)`;
 
-
+//     carruselLi.forEach((cadaPunto, i) => {
+//       carruselLi[i].classList.remove("carrusel__li--activo");
+//     });
+//     carruselLi[i].classList.add("carrusel__li--activo");
+//   });
+  // if (posicion === 1) {
+  //   let operacion = posicion * -0;
+  //   carruselContainerimg.style.transform = `translateX(${operacion}%)`;
+  //   carruselGlobaltext.style.transform = `translateX(${operacion}%)`;
+  // }else{
+  //   let operacion = posicion * -50;
+  //   carruselContainerimg.style.transform = `translateX(${operacion}%)`;
+  //   carruselGlobaltext.style.transform = `translateX(${operacion}%)`;
+  // }
+// }, 4000);
 
 function showMenu() {
   nav.classList.toggle("activo");
